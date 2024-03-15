dcb3ffd45b11500aef48da449c09a8247e9e2026
Peter Nearing <me@peternearing.ca>
2024-03-15 12:33:25 -0400

    Start builder attempt

--- main.py
+++ main.py
@@ -3,5 +3,15 @@
     File: main.py
 """
 
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import GLib, Gio, Gtk
+
+
 if __name__ == '__main__':
-    exit(0)
+    builder = Gtk.Builder()
+    builder.add_from_file("ClusterEncode.glade")
+    window = builder.get_object("app_window")
+    window.show_all()
+
+    Gtk.main()
